139bd0d5b5f0df110c3713fba310a535fae431ae
MananDharsandiya <109237759+MananDharsandiya@users.noreply.github.com>
2025-03-31 00:11:01 +0530

    Add files via upload

---
+++ framework/views/83f3cd3ec3e788d70f3c85544b89116a.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>3D Colorful Room</title>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/js/3droom.js'); ?>
+    <style>
+        body { margin: 0; overflow: hidden; background-color: black; }
+    </style>
+</head>
+<body>
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\3DAnimationProject\resources\views/3droom.blade.php ENDPATH**/ ?>
